e8a483497698b61cc8c6f3b949c75f5e8c06f608
Greyson Gerhard-Young <greyson_gerhard-young@brown.edu>
2019-07-25 14:38:38 -0700

    Updates

---
+++ precache-manifest.33c34b1239838b9538b31db8e5ad9815.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57ceaa9854a006818042af7704b178f1",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "ab2605693fc5bc68f781",
+    "url": "/clickyGame/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/clickyGame/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ab2605693fc5bc68f781",
+    "url": "/clickyGame/static/js/main.31269153.chunk.js"
+  },
+  {
+    "revision": "95f5ce002aae55a30c31",
+    "url": "/clickyGame/static/js/runtime~main.d5f62368.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clickyGame/static/media/logo.5d5d9eef.svg"
+  }
+]);
